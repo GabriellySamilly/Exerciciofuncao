60fd59fbf17244537af60a215ccb21ffb391f177
GabriellySamilly <166051480+GabriellySamilly@users.noreply.github.com>
2024-04-05 17:54:09 +0000

    modifiquei o arquivo atividade.js e adicionei um novo arquivo de informação

--- atividade.js
+++ atividade.js
@@ -1,4 +1,8 @@
-//function expression
-const soma = function(){
-    return a+ b
+function soma(valor1,valor2){
+    const resultado = valor1 + valor2
+    if(resultado>100){
+        console.log(resultado)
+    }
 }
+
+soma(90, 50)
